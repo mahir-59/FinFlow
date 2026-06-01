d11a89b3c86d0a8acf6f228303af65dc6e2d6678
mahir-59 <tmahirali222@gmail.com>
2026-06-01 23:59:46 +0530

    All code merge

---
+++ FinFlow.App/Components/Pages/Auth/CreatePassword.razor.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinFlow.App.Components.Pages.Auth
+{
+    public partial class CreatePassword : ComponentBase
+    {
+        private string Password = "";
+        private string ConfirmPassword = "";
+
+        private bool ShowPassword;
+        private bool ShowConfirmPassword;
+
+        private void TogglePassword()
+        {
+            ShowPassword = !ShowPassword;
+        }
+
+        private void ToggleConfirmPassword()
+        {
+            ShowConfirmPassword = !ShowConfirmPassword;
+        }
+
+        private int StrengthLevel
+        {
+            get
+            {
+                int score = 0;
+
+                if (Password.Length >= 8)
+                    score++;
+
+                if (Password.Any(char.IsUpper))
+                    score++;
+
+                if (Password.Any(char.IsDigit))
+                    score++;
+
+                if (Password.Any(ch => !char.IsLetterOrDigit(ch)))
+                    score++;
+
+                return score;
+            }
+        }
+
+        private string StrengthText =>
+    StrengthLevel switch
+    {
+        1 => "Weak",
+        2 => "Fair",
+        3 => "Good",
+        4 => "Strong",
+        _ => ""
+    };
+
+        private string StrengthClass =>
+            StrengthLevel switch
+            {
+                1 => "weak",
+                2 => "fair",
+                3 => "good",
+                4 => "strong",
+                _ => ""
+            };
+    }
+}
--- FinFlow.App/Components/Pages/Auth/VerifyOtp.razor.cs
+++ FinFlow.App/Components/Pages/Auth/VerifyOtp.razor.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
-namespace FinFlow.App.Components.Pages.Auth
+namespace FinFlow.App.Components.Pages.Auth;
+
+public partial class VerifyOtp
 {
-    public partial class VerifyOtp : ComponentBase
-    {
+    [Inject] private IJSRuntime JS { get; set; } = default!;
 
+    private ElementReference _otpInput;
+
+    private string OtpValue = string.Empty;
+
+    private async Task HandleOtpInput(ChangeEventArgs e)
+    {
+        var value = e.Value?.ToString() ?? "";
+
+        value = new string(value.Where(char.IsDigit).ToArray());
+
+        if (value.Length > 6)
+            value = value[..6];
+
+        OtpValue = value;
+
+        StateHasChanged();
+
+        if (OtpValue.Length == 6)
+        {
+            // Verify OTP API call here
+        }
+    }
+
+    private async Task FocusInput()
+    {
+        await JS.InvokeVoidAsync("focusElement", _otpInput);
     }
 }
