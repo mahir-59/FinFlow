d6a20e088a4d71fd77415c7fdfdc3453ba0ee0d3
mmirapakayala <mmirapakayala@americanfirstfinance>
2026-06-17 11:48:03 +0530

    d

--- FinFlow.Modules/Items/Items.BL/Interfaces/IItemBL.cs
+++ FinFlow.Modules/Items/Items.BL/Interfaces/IItemBL.cs
@@ -1,6 +1,7 @@
 
 using FinFlow.Modules.Items.Items.DL.Classes;
 using FinFlow.Modules.Items.Items.Model.Classes;
+using FinFlow.Modules.Items.Items.Model.Interfaces;
 
 namespace FinFlow.Modules.Items.Items.BL.Interfaces
 {
